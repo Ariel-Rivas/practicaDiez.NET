d741bee943965cc409416817e1ee708de7890c14
Ariel <narivas@gylgroup.com>
2022-12-11 17:52:15 -0300

    unico ejercicio de la practica diez

---
+++ PracticaDiez-ejer-01/ConsoleApp1/ConsoleApp1/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; 
+namespace ConsoleApp1
+{
+    class Animal : IAnimal 
+    {
+      
+     
+        public void Andar()
+        {
+            WriteLine("Ya estoy caminando");
+        }
+
+        public bool EsPerro() 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Saltar()
+        {
+            WriteLine(" Estoy saltando");
+        }
+    }
+}
---
+++ PracticaDiez-ejer-01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+//1- Crea una Clase "Animal" que implemente la interfaz "IAnimal" , la interfaz "IAnimal" tendrá los métodos, Andar, es perro, saltar. 
+//implementa estos métodos en la clase. y usa esa clase en un programa de consola.
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal perro = new Animal();
+
+            perro.Andar();
+           
+            perro.Saltar();
+
+            ReadLine(); 
+        }
+    }
+}
